553a7cc52ea0ab6878275a89a1f308c93a53b75b
eduardovisa <eduardo.mvisa@gmail.com>
2023-05-27 22:22:49 -0600

    Swagger installation

--- bin/index.js
+++ bin/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const bodyparser = require('body-parser');
+const swaggerUi = require('swagger-ui-express');
 const routes = require('./routes/book');
+const swaggerDocument = require('../swagger.json');
 
 const app = express();
 const port = 3000;
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(bodyparser.json());
-
 app.use(routes);
 
 app.listen(port, () => {
